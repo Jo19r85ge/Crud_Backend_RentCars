842786c51f387e352006b492b0cb3b6134831a94
Jorge_JJG <48150256+Jo19r85ge@users.noreply.github.com>
2019-08-10 21:21:51 +0200

    Update ValuesController.cs

--- RentCars.Web/Controllers/ValuesController.cs
+++ RentCars.Web/Controllers/ValuesController.cs
@@ -11,6 +11,7 @@ namespace RentCars.Web.Controllers
     public class ValuesController : ControllerBase
     {
         // GET api/values
+        
         [HttpGet]
         public ActionResult<IEnumerable<string>> Get()
         {
@@ -18,6 +19,7 @@ namespace RentCars.Web.Controllers
         }
 
         // GET api/values/5
+        
         [HttpGet("{id}")]
         public ActionResult<string> Get(int id)
         {
@@ -25,18 +27,21 @@ namespace RentCars.Web.Controllers
         }
 
         // POST api/values
+        
         [HttpPost]
         public void Post([FromBody] string value)
         {
         }
 
         // PUT api/values/5
+        
         [HttpPut("{id}")]
         public void Put(int id, [FromBody] string value)
         {
         }
 
         // DELETE api/values/5
+        
         [HttpDelete("{id}")]
         public void Delete(int id)
         {
