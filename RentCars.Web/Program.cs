b17cc26c49b6a922fea97e85851140116bf25a27
Jorge_JJG <48150256+Jo19r85ge@users.noreply.github.com>
2020-01-15 22:59:01 +0100

    Update Program.cs

--- RentCars.Web/Program.cs
+++ RentCars.Web/Program.cs
@@ -20,5 +20,6 @@ namespace RentCars.Web
         public static IWebHostBuilder CreateWebHostBuilder(string[] args) =>
             WebHost.CreateDefaultBuilder(args)
                 .UseStartup<Startup>();
+        
     }
 }
